ebb6037188297a1763919c5b1e477bb7ffc634e8
Mike Roberts <quantumlemur@gmail.com>
2013-02-18 20:29:40 -0500

    got it running

--- BEMT.py
+++ BEMT.py
@@ -64,7 +64,7 @@ class Blade:
         self.twist = interpolate.splev(self.r, twistspl)
         twist75 = interpolate.splev(.75, twistspl)
         self.twist -= twist75
-        self.theta_tw = tipTwist
+        self.theta_tw = tipTwist * math.pi/180.
         chordspl = interpolate.splrep(np.array([rootCutout,1]), np.array([rootChord,rootChord*taperRatio]), k=1)  # k=1 for linear interpolation
         self.chord = interpolate.splev(self.r, chordspl)
 
@@ -110,7 +110,7 @@ class Blade:
 class Rotor:
     """This class holds the rotor and actually deals with trimming it and whatnot.
 
-    It contains dinensional quantities."""
+    It contains dimensional quantities."""
     
     def __init__(self, blade, psiSegments, omega, radius, numblades):
         self.blade = blade
@@ -118,6 +118,7 @@ class Rotor:
         self.radius = radius
         self.R = radius
         self.numblades = numblades
+        self.Vtip = omega * (2*math.pi*radius)
         # set up 1D arrays of psi and r...
         self.psi1D, self.dpsi = np.linspace(0, 2*math.pi, psiSegments, endpoint=False, retstep=True)
         r1D = self.blade.r
@@ -128,8 +129,6 @@ class Rotor:
         self.chord = self.blade.chord * radius
         self.r = self.blade.r * radius
         self.dr = self.blade.dr * radius
-        # set the pitch values
-        self.setPitch(pitch)
         # precompute and store the values of sin(psi) and cos(psi) since they're static and we're going to use them a lot later on
         self.cospsi = np.cos(self.psi)
         self.sinpsi = np.sin(self.psi)
@@ -141,12 +140,6 @@ class Rotor:
         self.theta_1c = 9999999.
         self.theta_1s = 9999999.
         self.inflow = 9999999.
-    
-    def setPitch(self, pitch):
-        self.pitch = pitch
-        self.theta_0 = pitch
-        theta1D = pitch + self.blade.twist
-        self.thetaFixed, psi = np.meshgrid(theta1D, self.psi1D)
 
     def reinitializeTrimVars(self, Fx, Fz, rho, V):
         # We'll just use .1 rad = 5.73 deg to initialize for now.  It'd be better to use closed-form estimates, but we'll add that later.
@@ -156,6 +149,8 @@ class Rotor:
         self.theta_0 = .1
         self.theta_1c = .1
         self.theta_1s = .1
+        self.A = .1
+        self.B = .1
         # inflow calculations
         self.alpha_tpp = math.tan(Fx/Fz)
         thrust = math.sqrt(Fx**2 + Fz**2)
@@ -182,41 +177,62 @@ class Rotor:
         numblades = self.numblades
         cospsi = self.cospsi
         sinpsi = self.sinpsi
+        Vtip = self.Vtip
+        blade = self.blade
+        mu = V / Vtip
+        lockNumber = 8 # assumed estimate
         alpha_TPP = math.tan(Fx/Fz) # tip path plane angle
 
         # First we'll check if the pre-existing trim solution is reasonable. If not, re-initialize them.
         if not((self.inflow<1000) and (self.beta_0>0) and (self.beta_0<math.pi/4) and (abs(self.beta_1c)<math.pi/4) and (abs(self.beta_1s)<math.pi/4) and (abs(self.theta_1c)<math.pi/4) and (abs(self.theta_1s)<math.pi/4)):
             self.reinitializeTrimVars(Fx=Fx, Fz=Fz, rho=rho, V=V)
         inflow = self.inflow
+        lambda_TPP = inflow / Vtip + mu*math.tan(alpha_TPP)
+        beta_0 = self.beta_0
         theta_0 = self.theta_0
-        theta_1c = self.theta_1c
-        theta_1s = self.theta_1s
-        F_x = 0
-        F_y = 0
-        F_z = 0
+        A = self.A
+        B = self.B
+        L = 0
         loops = 0
         if debug: print('TARGETS:     F_x=%d      F_y=%d      F_z=%d' % (Fx, Fy, Fz))
-        while (abs(F_x-Fx)>1 or abs(F_y-Fy)>1 or abs(F_z-Fz)>1) and loops<1000:
+        while (abs(Fz-L)>1) and loops<10000:
             loops += 1
             #### These following equations are from slides ~70-80 in Part2.ppt of AE6070 notes.  All angles relative to flight path?
             # find the effective blade section angle of attack
-            theta = self.thetaFixed + theta_1c*cospsi + theta_1s*sinpsi # local flow angle
+            beta = beta_0 # Ignore flapping; only use precone
             U_T = omega*r + V*sinpsi # local tangential velocity.  This assumes small shaft angle alpha_s
-            alphaEffective = 1/U_T * (omega*r*(theta_0 + blade.theta_tw*r/R + (theta_1c-beta_1s)*cospsi + (theta_1s+beta_1c)*sinpsi)*V*(theta_0 + blade.theta_tw*r/R)*sinpsi + V*(theta_1c-beta_1s)*cospsi*sinpsi + V*(theta_1s+beta_1c)*sinpsi**2 - V*beta_0*cospsi - V*alpha_TPP - inflow)
+            U_P = inflow + V*beta*cospsi # local perpendicular velocity.  This ignores flapping and also assumes small shaft angle alpha_s
+            # A = theta_1c-beta_1s
+            # B = theta_1s+beta_1c
+            alphaEffective = 1/U_T * (omega*r*(theta_0 + blade.theta_tw*r/R + (A)*cospsi + (B)*sinpsi)*V*(theta_0 + blade.theta_tw*r/R)*sinpsi + V*(A)*cospsi*sinpsi + V*(B)*sinpsi**2 - V*beta_0*cospsi - V*alpha_TPP - inflow)
             #### alphaEffective = thetaFixed - np.arctan((R*inflow*omega + r*omega*(beta_1s*cospsi-beta_1c*sinpsi) + V*math.cos(alpha_TPP-beta_1c)*cospsi*(beta_0 + beta_1c*cospsi + beta_1s*sinpsi)) / (r*omega + V*math.cos(alpha_TPP-beta_1c)*sinpsi)) + theta_1c*cospsi + theta_1s*sinpsi # This is the full equation for the line above.  Retained only for reference and troubleshooting.
             # get the cl, cd, and cm data for blade sections.  Since we're using lookup tables, this is a quasi-steady solution.  We may want to add in corrections for unstead aerodynamic effects and dynamic stall in the future.  Dynamic stall is also important to find the vibration levels of the vehicle in high speed forward flight.
             cl = self.blade.cl(alphaEffective)
             cd = self.blade.cd(alphaEffective)
             cm = self.blade.cm(alphaEffective)
             # apply Prandtl-Glauert compressibility correction
-            U_total = np.sqrt(U_T**2 + U_P**2)
-            mach = U_total / speedOfSound # Assumes U_T>>U_P, which is only untrue if U_T is small (i.e. on the retreating side in high-speed flight), in which case mach numbers will be small anyway, so should be ok.
+            U_total = np.sqrt(U_T**2 + U_P**2) # Although we do not, we can assume U_T>>U_P, which is only untrue if U_T is small (i.e. on the retreating side in high-speed flight), in which case mach numbers will be small anyway, so should be ok.
+            mach = U_total / speedOfSound
             cl = cl / np.sqrt(1-mach**2)
-            # Find the rotor sectional lift and drag
+            # Find the rotor sectional (1D) lift and drag
             dL = .5 * rho * chord * U_total**2 * cl
             dD = .5 * rho * chord * U_total**2 * cd
+            # Calculate the piecewise (2D) lift and drag
+            dL = dL * dr * dpsi
+            dD = dD * dr * dpsi
             # Integrate over the rotor surface
             T = np.sum(dL) * numblades
+            P = np.sum(dD * U_T) * numblades
+            # Calculate trim angles
+            beta_0 = lockNumber * ((theta_0+blade.theta_tw)/8*(1+mu**2) - mu**2/60*(theta_0+blade.theta_tw) - lambda_TPP/6 + mu*B/6)
+            B = -8./3.*mu*((theta_0+blade.theta_tw) - 3./4.*lambda_TPP) / (1 + 3./2.*mu**2)
+            A = (-4./3.*mu*beta_0) / (1. + .5*mu**2)
+            # Find vertical lift and adjust collective
+            L = T * math.cos(alpha_TPP)
+            liftDeficitPct = (Fz - L) / Fz
+            theta_0 -= liftDeficitPct / 100
+            # print debug output
+            if debug: pvar(locals(), ('Fz', 'L', 'theta_0', 'beta_0', 'A', 'B'))
         print loops
 
 
@@ -312,5 +328,5 @@ if __name__ == '__main__':
     b = Blade(c81File='Config/S809_Cln.dat', skip_header=12, skip_footer=1, rootChord=1./30, taperRatio=.8, tipTwist=-12., rootCutout=.2, segments=15)
     r = Rotor(b, psiSegments=15, omega=omega, radius=r, numblades=4)
     Fhorizontal = 1./2 * rho * V**2 * f
-    Fvertical = 15000. # pounds
+    Fvertical = 13000. # pounds
     r.trim(tolerance=.1, V=V, rho=rho, speedOfSound=1026., Fx=Fhorizontal, Fy=0, Fz=Fvertical)
